93fb461d5b40e6a37a90da06eb938f04677a355c
Glenn Olsson <glennholsson@gmail.com>
2021-08-23 18:53:56 +0200

    Add sims gui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import './App.css';
-import Webcam from 'react-webcam';
+import "./App.css";
+import Webcam from "react-webcam";
 
 const width = 1000;
 const height = 500;
 
 function App() {
+	return (
+		<div className="App">
+			<Webcam
+				style={{
+					position: "relative",
+					zIndex: 0,
+					alignSelf: "flex-start",
+				}}
+				videoConstraints={{ height, width }}
+			/>
 
-  return (
-    <div className="App"> 
-          <Webcam style={{position: 'relative', zIndex: 0, alignSelf: 'flex-start'}} videoConstraints={{height, width}}/>
-
-          <div style={{
-            backgroundColor: 'blue',
-            zIndex: 100,
-            position: 'relative',
-            height: 10,
-            width: 10
-          }} />
-    </div>
-  );
+			<div
+				style={{
+					backgroundColor: "blue",
+					zIndex: 100,
+					position: "relative",
+					height: 10,
+					width: 10,
+				}}
+			/>
+		</div>
+	);
 }
 
 export default App;
