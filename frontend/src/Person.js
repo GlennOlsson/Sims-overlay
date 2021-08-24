964be15264c5bbfc937fef27e2e14a159e8921f9
Glenn Olsson <glennholsson@gmail.com>
2021-08-24 21:10:49 +0200

    Set money to come from person
    Co-authored-by: Albin Winkelmann <albin-w@hotmail.com>

--- frontend/src/Person.js
+++ frontend/src/Person.js
@@ -14,6 +14,22 @@ export default function Person({ person }) {
 		environment
 	} = {...person.needs}
 
+	//Split up into parts of 3 chars and add §
+	const moneyStr = person.money + ""
+	
+	console.log(moneyStr)
+
+	let money = ""
+	for(var i = 0; i < moneyStr.length; i++) {
+		if(i !== 0 && i % 3 === 0)
+			money = " " + money
+		let index = moneyStr.length - i - 1;
+		console.log(money, i, index)
+		money = moneyStr[index] + money;
+	}
+
+	money = "§" + money
+
 	return (
 		<>
 			<Bar x={602} y={607} percentage={hunger} />
@@ -31,6 +47,23 @@ export default function Person({ person }) {
 			<Bar x={741} y={662} percentage={hygiene} />
 
 			<Bar x={741} y={689} percentage={environment} />
+
+			<p style={{
+					backgroundColor: "rgb(106,166,174)",
+					width: 73,
+					height: 14,
+					top: 673,
+					left: 19,
+					borderRadius: 30,
+					position: 'absolute',
+					fontSize: 10,
+					textAlign: 'center',
+					color: "rgb(0, 14, 77)",
+					verticalAlign: "middle",
+					paddingTop: 1
+				}}>
+					{money}
+				</p>
 		</>
 	)
 
--- frontend/src/SimsGui.js
+++ frontend/src/SimsGui.js
@@ -11,14 +11,13 @@ const height = 1080;
 
 const guiScaling = 0.5;
 
-const host = "http://8862-188-151-144-92.ngrok.io"
+const host = "http://a51e-188-151-144-92.ngrok.io"
 // const host = "http://localhost:8000"
 
 export default function SimsGui() {
 
 	let [selectedPerson, setSelectedPerson] = useState(null);
 	let [modalText, setModal] = useState(null);
-	let [money, setMoney] = useState("§86");
 
 	const fetchCurrentPerson = () => {
 		let url = host + "/current-character"
@@ -39,39 +38,20 @@ export default function SimsGui() {
 			})
 	}
 
-	const fetchMoney = () => {
-		let url = host + "/money"
-		return fetch(url)
-			.then(r => r.json())
-			.then(json => {
-				//Split up into parts of 3 chars and add §
-				let moneyStr = json["money"] + ""
-				
-				let money = ""
-				for(var i = moneyStr.length - 1; i >= 0; i--) {
-					money = moneyStr[i] + money;
-					if(i !== 0 && i % 3 === 0)
-						money = " " + money
-				}
-
-				setMoney("§" + money)
-			})
-	}
-
-	const refetch = () => {
-		setTimeout(() => {
-			Promise.all([
-				fetchCurrentPerson(),
-				fetchModal(),
-				fetchMoney()
-			]).then(() => {
-				// refetch()
-			})
-		}, 5000)
-	}
-
 	//Code only runs once, not every time the state is updated
-	useEffect(refetch, []);
+	useEffect(() => {
+		const refetch = () => {
+			setTimeout(() => {
+				Promise.all([
+					fetchCurrentPerson(),
+					fetchModal()
+				]).then(() => {
+					refetch()
+				})
+			}, 5000)
+		}
+		refetch()
+	}, [])
 
 	return (
 		<>
@@ -99,23 +79,6 @@ export default function SimsGui() {
 				/>
 
 				{ modalText && <Modal text={modalText} />}
-
-				<p style={{
-					backgroundColor: "rgb(106,166,174)",
-					width: 73,
-					height: 14,
-					top: 673,
-					left: 19,
-					borderRadius: 30,
-					position: 'absolute',
-					fontSize: 10,
-					textAlign: 'center',
-					color: "rgb(0, 14, 77)",
-					verticalAlign: "middle",
-					paddingTop: 1
-				}}>
-					{money}
-				</p>
 			</div>
 
 			{selectedPerson && <Person person={selectedPerson} />}
